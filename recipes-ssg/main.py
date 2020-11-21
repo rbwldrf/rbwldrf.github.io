7f8437340f49d3b22be287e607779a437fe29d9a
rbwldrf <rbwldrf@users.noreply.github.com>
2020-11-21 18:39:47 +0000

    fuck it

--- recipes-ssg/main.py
+++ recipes-ssg/main.py
@@ -1,23 +1,17 @@
 import os
 from markdown2 import markdown
-from jinja2 import Environment, PackageLoader
+import jinja2 as j
 
 print(os.getcwd())
 
-env = Environment(loader=PackageLoader("main","templates"))
+env = j.Environment(loader=j.FileSystemLoader("templates"))
 template = env.get_template("test.html")
 
 
 src = open("recipes-ssg/a.md","r")
 base = open("recipes/home.html","w")
 
-base.write(markdown("ughh"))
-
-data = {
-    'content': src,
-    'title': src.metadata['title'],
-    'date': src.metadata['date']
-    }
+base.write(markdown("er ennþá að reyna að læra á þetta"))
 
 src.close()
 base.close()
