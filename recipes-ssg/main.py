c4bc40d12de603d4017abb52c0ecd2754e3f0196
rbwldrf <rbwldrf@users.noreply.github.com>
2020-11-23 07:49:30 +0000

    test

--- recipes-ssg/main.py
+++ recipes-ssg/main.py
@@ -7,12 +7,14 @@ print(os.getcwd())
 env = j.Environment(loader=j.FileSystemLoader("templates"))
 template = env.get_template("test.html")
 
-
-
 src = open("recipes-ssg/a.md","r")
 base = open("recipes/home.html","w")
 
-base.write(template.render(bla=markdown("#welcome to my site \n _hello_")))
+base.write(template.render(
+    bla=markdown("#welcome to my site \n _hello_"),
+    recipes=[{"href":"a.com","desc":"hello"}]
+    )
+)
 
 src.close()
 base.close()
