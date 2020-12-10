6936a9f9143ba2317394a630c42944b8301b5195
rbwldrf <rbwldrf@users.noreply.github.com>
2020-12-10 06:23:07 +0000

    a

--- recipes-ssg/main.py
+++ recipes-ssg/main.py
@@ -9,6 +9,7 @@ env = j.Environment(loader=j.FileSystemLoader("templates"))
 
 template = env.get_template("test.html")
 recipe_template = env.get_template("recipe.html")
+table_template = env.get_template("table.html")
 
 imgpath = "./recipes-ssg/img"
 
@@ -23,17 +24,23 @@ base = open("myrecipes/index.html","w")
 
 #title = markdown("#"+"welcome to my recipe site!")
 
-mdpath = "./recipes-ssg/mds/recipes"
-mdpath2 = "./recipes-ssg/mds"
+mdpath = "./recipes-ssg/mds"
 
 f = list()
+f2 = list()
 
-for md in os.listdir(mdpath):
-    f.append(markdown(open(os.path.join(mdpath+"/"+md)).read()))
+
+for md in os.listdir(os.path.join(mdpath+"/recipes")):
+    f.append(markdown(open(os.path.join(mdpath+"/recipes/"+md)).read()))
+
+for md in os.listdir(os.path.join(mdpath+"/foods")):
+    f2.append(markdown(open(os.path.join(mdpath+"/foods/"+md)).read()).splitlines())
 
 rd_rec = list()
 
 recipes = list()
+foodlist = list()
+
 contentDictArray = [
     {"def":"center"},
         
@@ -43,10 +50,16 @@ contentDictArray = [
 for aa in f:
     recipes.append( recipe_template.render(content=aa) )
 
+for aa in f2:
+    foodlist.append( table_template.render(type=aa[0],size=aa[1],cost=aa[2]) )
+
+
+
 base.write(template.render(
     content=contentDictArray,
     rec=recipes,
-    about=markdown(open(os.path.join(mdpath2+"/about.md")).read())
+    foods=foodlist,
+    about=markdown(open(os.path.join(mdpath+"/about.md")).read())
     )
 )
 
